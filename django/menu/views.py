6891c163734ffe990c8014b8bbc09e69891507f7
KIM SHINEDAY <kimshineday99@gmail.com>
2025-06-20 20:18:11 +0900

    💡 DB 구성 변경
    카태고리 테이블 생성

--- django/menu/views.py
+++ django/menu/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-from .models import Menu
+from .models import Menu, Category
 
 def totalmenu_view(request):
-    all_menus = Menu.objects.all() # 등록된 전체 메뉴 목록
-    return render(request, 'totalmenu_view.html', {'menus': all_menus})
+    all_category = Category.objects.all().order_by('code') # 카테고리 목록
+    all_food = Menu.objects.all() # 음식 목록
+    return render(request, 'totalmenu_view.html', {'menus': all_food, 'category': all_category})
