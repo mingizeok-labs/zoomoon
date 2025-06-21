5ae76d9d29dbbecd92142271228e0b9e4a0669e4
KIM SHINEDAY <kimshineday99@gmail.com>
2025-06-21 22:55:30 +0900

    ✨ Feat.menu_add
    메뉴 추가 플랫폼 구축 중

--- django/menu/urls.py
+++ django/menu/urls.py
@@ -4,5 +4,6 @@ from . import views # menu/views.py
 app_name = 'menu' # name space 지정
 
 urlpatterns = [
-    path('totalmenu_view', views.totalmenu_view, name='totalmenu_view')
+    path('totalmenu_view', views.totalmenu_view, name='totalmenu_view'),
+    path('add', views.menu_add, name='menu_add'),
 ]
--- django/menu/views.py
+++ django/menu/views.py
@@ -7,3 +7,9 @@ def totalmenu_view(request):
     all_category = Category.objects.all().order_by('code') # 카테고리 목록
     all_food = Menu.objects.all() # 음식 목록
     return render(request, 'totalmenu_view.html', {'menus': all_food, 'category': all_category})
+
+def menu_add(request):
+    all_category = Category.objects.all()
+    # add_food = input() # 등록할 음식
+    # food_category = input() # 음식 카테고리 설정
+    return render(request, 'add_menu.html', {'category': all_category})
