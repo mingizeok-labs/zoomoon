8319bde19d8495b34f3d23b1688ca0cfdc9df816
KIM SHINEDAY <kimshineday99@gmail.com>
2025-06-24 22:30:26 +0900

    💡 Menu_Add
    db 연결 중..

--- django/menu/views.py
+++ django/menu/views.py
@@ -17,15 +17,14 @@ def add_menu(request):
     if request.method == 'GET':
         context = {'msg1' : '알잘딱깔센! 카테고리 선택과 메뉴 이름을 작성해주세요 :)', 'category': all_category}
         return render(request, 'add_menu.html', context)
-
     elif request.method == 'POST':
         category_id = request.POST.get('category_id')
         food = request.POST.get('food')
-        
-        if category_id and food: # 데이터 존재 여부 파악
+        if not category_id and food: # 데이터 존재 여부 파악
             select_cat = Category.objects.get(id=category_id)
             Menu.objects.create(food=food, category=select_cat)
-            return redirect('success_page_or_menu_list') # 리다이렉트할 URL 이름으로 변경 (예: 'menu_list')
+            print('f {음식 메뉴추가 완료')
+            return redirect('menu:totalmenu_view') # 리다이렉트할 URL 이름으로 변경 
         else: # 누락 시
             context = {'errormsg' : "메뉴 이름과 카테고리를 모두 입력해주세요."}
             return render(request, 'add_menu.html', context)
