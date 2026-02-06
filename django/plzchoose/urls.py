375c652e3c5230f2bb64614dc1d2ef55ced517d5
KIM SHINEDAY <kimshineday99@gmail.com>
2026-02-06 23:23:36 +0900

    ✨ 랜덤 메뉴 추천 페이지
    - service 로직 및 view 작성. 라우터 등록 및 html 작성

--- django/plzchoose/urls.py
+++ django/plzchoose/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views # plzchoose/view.py
 
+app_name = 'plzchoose'
+
 urlpatterns = [
-    path('', views.random, name='random')
+    path('', views.random_menu, name='random')
 ]
--- django/plzchoose/views.py
+++ django/plzchoose/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from .services import RandomMenuSelectService
+
 # Create your views here.
 def random(request):
     if request.method == 'GET':
@@ -7,3 +9,15 @@ def random(request):
             'msg':'test 성공'
         }
         return render(request, 'plzchoose.html', context)
+    
+
+def random_menu(request):
+    if request.method == 'POST':
+        random = RandomMenuSelectService()
+        menu = random.random_menu_select()
+
+        context = {'category': menu.category,
+                   'food': menu.food}
+        return render(request, 'plzchoose.html', context)
+    
+    return render(request, 'plzchoose.html')
